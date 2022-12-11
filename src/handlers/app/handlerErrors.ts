83a7ac1343ee1ff5d9b1537042152312e9b5d335
Metehan Akbaba <metehan@wesrom.com>
2022-12-11 22:29:36 +0300

    update unnecessary @return parameter

--- src/handlers/app/handlerErrors.ts
+++ src/handlers/app/handlerErrors.ts
@@ -5,7 +5,6 @@ import { ErrorType, IAxiosError, IStockError } from '~/src/types/app/IErrors'
  * I created some middleware based on this structure to make the code a bit cleaner in the eventual request
  *
  * @param callback {(err: IAxiosError<T> | IStockError<T>)}
- * @return {(error: (Error | AxiosErrorGenerics<T>)) => void}
  */
 export default function handlerErrors<T>(callback: (err: IAxiosError<T> | IStockError<T>) => void) {
   return (error: Error | AxiosErrorGenerics<T>): void => {
